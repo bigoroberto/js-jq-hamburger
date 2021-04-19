cce49c3d40829734a5b7d32d399763946fa22cf1
bigoroberto <bigoroberto@gmail.com>
2021-04-19 16:49:56 +0200

    esercizio FINITO FUNZIONANTE

--- hamburger-menu-base/assets/js/main.js
+++ hamburger-menu-base/assets/js/main.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-  
   //creo le mie referenze
   var listHamburgerMenu = $(".hamburger-menu");
-  var menuBurger = $(".header > a ");
+  var menuBurger = $(".header-right > a ");
   var closeMenuBurger = $(".close");
   
   //do il comando click al menuburger per far apparire il suo contenuto
   menuBurger.click(function(){
     listHamburgerMenu.show();
   });
-
   //do il comando click al menuburger per far chiudere il suo contenuto
   closeMenuBurger.click(function(){
     listHamburgerMenu.hide();
